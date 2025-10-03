eedcf7f42605a22b48ced7b9730efd54dfbbbe59
Jonathas Pereira <pjonathas972@gmail.com>
2025-10-03 19:04:52 -0300

    Demais exercicios jornada de dados

--- exercicios.py
+++ exercicios.py
@@ -11,7 +11,28 @@ import math
 
 ##10. Escreva um programa que calcula a área de um írculo, recebendo o raio como entrada.
 
-raio_indicado = float(input("Digite o raio do círculo: "))
-area_indicada = 2 * math.pi * raio_indicado
+#raio_indicado = float(input("Digite o raio do círculo: "))
+#area_indicada = 2 * math.pi * raio_indicado
 
-print(f"{area_indicada:.2f}")
+#print(f"{area_indicada:.2f}")
+
+#data_usuario = input("Digite uma data no formato dd/mm/aaaa: ")
+#lista_dia_mes_ano = data_usuario.split("/")
+#print(f"Dia: {lista_dia_mes_ano[0]}")
+#print(f"Mês: {lista_dia_mes_ano[1]}")
+#print(f"Ano: {lista_dia_mes_ano[2]}")
+
+#numero = int(input("Digite um número inteiro: "))
+#if isinstance(numero, int ):
+   # print("O número digitado é um número inteiro.") 
+#else:
+    #print("O número digitado não é um número inteiro.")
+
+idade = 18
+
+if idade < 18:
+    print("Menor de idade")
+elif idade == 18:
+    print("Tem 18 anos")
+else:
+    print("Maior de idade")
